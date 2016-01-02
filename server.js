606258a93554943ec603a55b10a7ac1df6cf5a1d
Yuriy Shefer <sheff@sheffmachine.com>
2016-01-02 16:36:34 -0600

    add todos and todos id routing and API responses

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 7000;
+var todos = [{
+    id: 0,
+    description: 'Learn node',
+    completed: false
+}, {
+    id: 1,
+    description: 'Learn Mongo DB',
+    completed: false
+}, {
+    id: 2,
+    description: 'Learn Postman',
+    completed: true
+}];
 
 app.get('/', function(req, res) {
     res.send('Todo API root');
+});
+
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    todos.forEach(function (todo) {
+        if (todo.id === todoId) {
+            matchedTodo = todo;
+        }
+    });
+    if (matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+//    res.json(todos);
 });
 
 app.listen(port, function() {
