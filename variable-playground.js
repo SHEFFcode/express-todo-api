400efeb0e0cdfdb82d77377e00ecab38d70c54cd
Yuriy Shefer <sheff@sheffmachine.com>
2016-01-02 20:34:01 -0600

    add the ?completed filter to the API

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+//var person = {
+//    name: 'Andrew',
+//    age: '21'
+//}
+//
+//function updatePerson (obj) {
+//    obj.age = 24;
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+//Array example
+
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+    gradesArr.push(55);
+    debugger;
+}
+
+addGrades(grades);
+console.log(grades);
