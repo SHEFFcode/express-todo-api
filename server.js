a2bf8e3be0e00b52c0e2283d0cc8e4d1b2c244ad
Yuriy Shefer <sheff@sheffmachine.com>
2016-01-02 19:08:26 -0600

    Add delete todos route

--- server.js
+++ server.js
@@ -38,6 +38,16 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
+app.delete('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    if (!matchedTodo) {
+        res.status(404).send();
+    }
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+});
+
 app.listen(port, function() {
    console.log('exoress listening on port ' + port + '!') 
 });
